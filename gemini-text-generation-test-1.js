6c0b27102bfeef63969e6291e2f65fa2aee798b8
Avinash Gupta <avinash.gupta@neutrinos.co>
2024-03-31 18:52:48 +0530

    get grmini key from env

--- gemini-text-generation-test-1.js
+++ gemini-text-generation-test-1.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI('TOKEN');
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
 
 (async () => {
@@ -9,7 +9,7 @@ const genAI = new GoogleGenerativeAI('TOKEN');
         // For text-only input, use the gemini-pro model
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-        const prompt = "Write a story about a love"
+        const prompt = "I want to write blogpost for Future of AI and Human birth. for this just provide an array of titles that can be used."
 
         const result = await model.generateContent(prompt);
         const response = await result.response;
